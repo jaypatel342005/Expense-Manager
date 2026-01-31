c69674ab11147fcebf3fc6e918a24e7a78f14c2a
jaypatel342005 <pateljay97378@gmail.com>
2026-01-31 14:39:26 +0530

    11:51 am , Saturday, 31 January 2026

--- src/app/(dashboard)/admin/people/new/page.tsx
+++ src/app/(dashboard)/admin/people/new/page.tsx
@@ -1,14 +1,14 @@
 // import { auth } from "@/auth";
-// import { redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 // import PeopleForm from "@/components/forms/people-form";
 
-// export default async function NewPeoplePage() {
-//     const session = await auth();
-//     if (!session?.user) return redirect("/sign-in");
+export default async function NewPeoplePage() {
+    // const session = await auth();
+    if (!session?.user) return redirect("/sign-in");
 
-//     return (
-//         <div className="container mx-auto py-10">
-//             <PeopleForm />
-//         </div>
-//     );
-// }
+    return (
+        <div className="container mx-auto py-10">
+            {/* <PeopleForm /> */}
+        </div>
+    );
+}
