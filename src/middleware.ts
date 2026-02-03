c14b1095b13b1cdbcc5297f949d0723345b8a110
jaypatel342005 <pateljay97378@gmail.com>
2026-02-03 20:10:40 +0530

    8:09 pm , Tuesday, 3 February 2026

--- src/middleware.ts
+++ src/middleware.ts
@@ -27,6 +27,7 @@ export default async function middleware(req: NextRequest) {
   return NextResponse.next()
 }
 
+
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 }
